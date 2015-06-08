6d232679732fac45b9e731b3409ccdb636ae48a6
Sameh R. Labib <schoolbuscompany@gmail.com>
2015-06-08 12:49:41 -0400

    Added user.php and transferring to dev

--- web/helpme/test.php
+++ web/helpme/test.php
@@ -1,7 +1,8 @@
 <?php
 //Script to test whether MySQLDatabase.php connects to the database.
 
-require_once('../../includes/MySQLDatabase.php');
+require_once("../../includes/MySQLDatabase.php");
+require_once("../../includes/user.php");
 
 if(isset($database)) { echo "true"; } else { echo "false"; }
 echo "<br />";
@@ -16,5 +17,18 @@ $sql = "SELECT * FROM users WHERE id=1";
 $result = $database->query($sql);
 $found_user = $database->fetch_array($result);
 echo $found_user['username'];
+
+echo "<hr />";
+
+$found_user = User::find_by_id(1);
+echo $found_user['username'];
+
+echo "<hr />";
+
+$user_set = User::find_all();
+while ($user = $database->fetch_array($user_set)) {
+  echo "User: ". $user['username'] ."<br />";
+  echo "Name: ". $user['first_name'] . " " . $user['last_name'] ."<br /><br />";
+}
 
 ?>
