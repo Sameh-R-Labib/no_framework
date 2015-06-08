246e4ffee758b1fb7649e3e12d64063d3a370db0
Sameh R. Labib <schoolbuscompany@gmail.com>
2015-06-08 10:56:51 -0400

    Commented out lines which insert on test.php

--- web/helpme/test.php
+++ web/helpme/test.php
@@ -1,4 +1,6 @@
 <?php
+//Script to test whether MySQLDatabase.php connects to the database.
+
 require_once('../../includes/MySQLDatabase.php');
 
 if(isset($database)) { echo "true"; } else { echo "false"; }
@@ -6,9 +8,9 @@ echo "<br />";
 
 echo $database->escape_value("It's working?<br />");
 
-$sql  = "INSERT INTO users (id, username, password, first_name, last_name) ";
-$sql .= "VALUES (1, 'kskoglund', 'secretpwd', 'Kevin', 'Skoglund')";
-$result = $database->query($sql);
+//$sql  = "INSERT INTO users (id, username, password, first_name, last_name) ";
+//$sql .= "VALUES (1, 'kskoglund', 'secretpwd', 'Kevin', 'Skoglund')";
+//$result = $database->query($sql);
 
 $sql = "SELECT * FROM users WHERE id=1";
 $result = $database->query($sql);
