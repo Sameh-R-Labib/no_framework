a58116c99413e22c2942d4a0f9f3a657c57ff70f
Sameh R. Labib <schoolbuscompany@gmail.com>
2015-06-08 10:49:03 -0400

    uncommented lines which insert into database

--- web/helpme/test.php
+++ web/helpme/test.php
@@ -6,13 +6,13 @@ echo "<br />";
 
 echo $database->escape_value("It's working?<br />");
 
-// $sql  = "INSERT INTO users (id, username, password, first_name, last_name) ";
-// $sql .= "VALUES (1, 'kskoglund', 'secretpwd', 'Kevin', 'Skoglund')";
-// $result = $database->query($sql);
+$sql  = "INSERT INTO users (id, username, password, first_name, last_name) ";
+$sql .= "VALUES (1, 'kskoglund', 'secretpwd', 'Kevin', 'Skoglund')";
+$result = $database->query($sql);
 
-//$sql = "SELECT * FROM users WHERE id=1";
-//$result = $database->query($sql);
-//$found_user = $database->fetch_array($result);
-//echo $found_user['username'];
+$sql = "SELECT * FROM users WHERE id=1";
+$result = $database->query($sql);
+$found_user = $database->fetch_array($result);
+echo $found_user['username'];
 
 ?>
