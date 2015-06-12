d46038862fb207c0f664c73d62787c80a25594c7
Sameh R. Labib <schoolbuscompany@gmail.com>
2015-06-12 11:06:48 -0400

    Fixed name mysqldatabase.php in databaseobject.php

--- includes/databaseobject.php
+++ includes/databaseobject.php
@@ -1,7 +1,7 @@
 <?php
 // If it's going to need the database, then it's 
 // probably smart to require it before we start.
-require_once(LIB_PATH.DS.'database.php');
+require_once(LIB_PATH.DS.'mysqldatabase.php');
 
 class DatabaseObject {
 
