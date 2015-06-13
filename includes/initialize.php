6e5ae1472e1c02b97452cd25b62a68967ec6d4a2
Sameh R. Labib <schoolbuscompany@gmail.com>
2015-06-13 13:30:06 -0400

    Defined LOGS_PATH in initialize.php

--- includes/initialize.php
+++ includes/initialize.php
@@ -1,9 +1,10 @@
 <?php
+///////
 // Here we define the core paths. Define them as absolute paths to
 // make sure that require_once works as expected. Also, we do as
 // many includes as we can to avoid having them coded into our
 // other scripts.
-
+///////
 
 // load config file first
 // config.php is the only file that differs from one installation
@@ -12,19 +13,28 @@
 // config.php and initialize.php must live in the same directory.
 require_once('config.php');
 
-
+///////
 // DEFINE PATH CONSTANTS
+///////
+
+// PROJ_ROOT is defined in config.php
 
 // DIRECTORY_SEPARATOR is a PHP pre-defined constant
 // (\ for Windows, / for Unix)
+//
 // DS
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
 // LIB_PATH
 defined('LIB_PATH') ? null : define('LIB_PATH', PROJ_ROOT.DS.'includes');
 
+// LOGS_DIR
+defined('LOGS_PATH') ? null : define('LOGS_PATH', PROJ_ROOT.DS.'logs');
 
+
+///////
 // INCLUDE LIBRARY SCRIPTS
+///////
 
 // load basic functions next so that everything after can use them
 require_once(LIB_PATH.DS.'functions.php');
