9ec203d2ca081f22be7bedfd5a2e354275bee2f8
Sameh R. Labib <schoolbuscompany@gmail.com>
2015-06-13 18:46:19 -0400

    View and clear logfile feature added

---
+++ web/helpme/admin/logfile.php
@@ -0,0 +1,52 @@
+<?php
+require_once("../../../includes/initialize.php");
+
+if (!$session->is_logged_in()) {
+  redirect_to("login.php");
+}
+
+$logfile = LOGS_PATH.DS.'log.txt';
+  
+if($_GET['clear'] == 'true') {
+  file_put_contents($logfile, '');
+  // Add the first log entry
+  log_action('Logs Cleared', "by User ID {$session->user_id}");
+  // redirect to this same page so that the URL won't 
+  // have "clear=true" anymore
+  redirect_to('logfile.php');
+}
+
+include_layout_template('admin_header.php');
+
+/////
+// HTML
+/////
+?>
+
+<a href="index.php">&laquo; Back</a><br /><br />
+
+<h2>Log File</h2>
+
+<p><a href="logfile.php?clear=true">Clear log file</a><p>
+
+
+<?php
+/////
+// PHP
+/////
+
+if (file_exists($logfile) && is_readable($logfile) && $handle = fopen($logfile, 'r')) {
+  echo "<ul class=\"log-entries\">";
+  while(!feof($handle)) {
+    $entry = fgets($handle);
+    echo "<li>{$entry}</li>";
+  }
+  echo "</ul>";
+  fclose($handle);
+} else {
+  echo "Could not read from {$logfile}.";
+}
+
+php include_layout_template('admin_footer.php');
+
+?>
