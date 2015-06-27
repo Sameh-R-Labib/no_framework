06664148a8673d1b49f1a1292a916c02c0ee851d
Sameh R. Labib <schoolbuscompany@gmail.com>
2015-06-27 15:38:59 -0400

    Added attribs and attach_file() to Photograph

--- includes/photograph.php
+++ includes/photograph.php
@@ -10,7 +10,50 @@ class Photograph extends DatabaseObject {
   public $type;
   public $size;
   public $caption;
-	
+
+  private $temp_path;
+  protected $upload_dir="images";
+  public $errors=array();
+
+
+  protected $upload_errors = array(
+    // http://www.php.net/manual/en/features.file-upload.errors.php
+    UPLOAD_ERR_OK 		=> "No errors.",
+    UPLOAD_ERR_INI_SIZE  	=> "Larger than upload_max_filesize.",
+    UPLOAD_ERR_FORM_SIZE 	=> "Larger than form MAX_FILE_SIZE.",
+    UPLOAD_ERR_PARTIAL 	        => "Partial upload.",
+    UPLOAD_ERR_NO_FILE          => "No file.",
+    UPLOAD_ERR_NO_TMP_DIR       => "No temporary directory.",
+    UPLOAD_ERR_CANT_WRITE       => "Can't write to disk.",
+    UPLOAD_ERR_EXTENSION        => "File upload stopped by extension."
+  );
+
+
+  /**
+   * Pass in $_FILE(['uploaded_file']) as an argument
+   **/
+  public function attach_file($file) {
+    // Perform error checking on the form parameters
+    if (!$file || empty($file) || !is_array($file)) {
+      // error: nothing uploaded or wrong argument usage
+      $this->errors[] = "No file was uploaded.";
+      return false;
+    } elseif ($file['error'] != 0) {
+      // error: report what PHP says went wrong
+      $this->errors[] = $this->upload_errors[$file['error']];
+      return false;
+    } else {
+      // Set object attributes to the form parameters.
+      $this->temp_path  = $file['tmp_name'];
+      $this->filename   = basename($file['name']);
+      $this->type       = $file['type'];
+      $this->size       = $file['size'];
+      // Don't worry about saving anything to the database yet.
+      return true;
+    }
+  }
+
+
 }
 
 ?>
