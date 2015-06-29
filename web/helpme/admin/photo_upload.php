fc13f087e83fb0c9571ea1654e6f58002a131281
Sameh R. Labib <schoolbuscompany@gmail.com>
2015-06-29 10:30:31 -0400

    Made sure photo_upload.php conforms to my project

--- web/helpme/admin/photo_upload.php
+++ web/helpme/admin/photo_upload.php
@@ -1,6 +1,7 @@
 <?php
-  require_once('../../includes/initialize.php');
+  require_once('../../../includes/initialize.php');
   if (!$session->is_logged_in()) { redirect_to("login.php"); }
+
   // The MAX_FILE_SIZE hidden field (measured in bytes) must precede
   // the file input field, and its value is the maximum filesize accepted
   // by PHP. This form element should always be used as it saves users the
@@ -41,10 +42,10 @@
   // <input name="userfile4" type="file" />
   // </form>
   $max_file_size = 318767104;   // expressed in bytes
-                              //     10240 =  10 KB
-                              //    102400 = 100 KB
-                              //   1048576 =   1 MB
-                              //  10485760 =  10 MB
+                                //     10240 =  10 KB
+                                //    102400 = 100 KB
+                                //   1048576 =   1 MB
+                                //  10485760 =  10 MB
   $message = "";
   if (isset($_POST['submit'])) {
     $photo = new Photograph();
@@ -59,13 +60,13 @@
     }
   }
 ?>
-  <?php include_layout_template('admin_header.php'); ?>
-  <h2>Photo Upload</h2>
-  <?php php echo output_message($message); ?>
-  <form action="photo_upload.php" enctype="multipart/form-data" method="POST">
-    <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max_file_size; ?>" />
-    <p><input type="file" name="file_upload" /></p>
-    <p>Caption: <input type="text" name="caption" value="" /></p>
-    <input type="submit" name="submit" value="Upload" />
-  </form>
-  <?php include_layout_template('admin_footer.php'); ?>
+<?php include_layout_template('admin_header.php'); ?>
+<h2>Photo Upload</h2>
+<?php php echo output_message($message); ?>
+<form action="photo_upload.php" enctype="multipart/form-data" method="POST">
+  <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $max_file_size; ?>" />
+  <p><input type="file" name="file_upload" /></p>
+  <p>Caption: <input type="text" name="caption" value="" /></p>
+  <input type="submit" name="submit" value="Upload" />
+</form>
+<?php include_layout_template('admin_footer.php'); ?>
