f39951e301587754424be9b5d3af217ecfa46f6b
Sameh R. Labib <schoolbuscompany@gmail.com>
2015-07-09 15:44:08 -0400

    Added sendemail.php to admin area

--- web/helpme/admin/index.php
+++ web/helpme/admin/index.php
@@ -10,8 +10,10 @@ if (!$session->is_logged_in()) { redirect_to("login.php"); }
 <?php echo output_message($message); ?>
 <ul>
   <li><a href="list_photos.php">List Photos</a></li>
+  <li><a href="photo_upload.php">Photo Upload</a></li>
   <li><a href="logfile.php">View Log file</a></li>
   <li><a href="logout.php">Logout</a></li>
+  <li><a href="sendemail.php">Test Email Sending</a></li>
 </ul>
 
 <?php include_layout_template('admin_footer.php'); ?>
---
+++ web/helpme/admin/sendemail.php
@@ -0,0 +1,29 @@
+<?php require_once("../../../includes/initialize.php"); ?>
+<?php if (!$session->is_logged_in()) { redirect_to("login.php"); } ?>
+
+<?php include_layout_template('admin_header.php'); ?>
+
+<a href="index.php">&laquo; Back</a><br />
+<br />
+
+<h2>Test Email Sending</h2>
+
+<?php echo output_message($message); ?>
+
+<?php
+
+
+  $curr_time = strftime("%T", time());
+  $to = "Sameh Labib <schoolbuscompany@gmail.com>";
+  $subject = "Mail Test at $curr_time";
+  $message = "This is a test.";
+  
+  $from = "SAMEH R Labib LLC <superuser@buscompanyx.com>";
+  $headers = "From: {$from}";
+  
+  $result = mail($to, $subject, $message, $headers);
+
+  echo $result ? "Sent: $curr_time" : 'Error: no email was sent';  
+?>
+
+<?php include_layout_template('admin_footer.php'); ?>
