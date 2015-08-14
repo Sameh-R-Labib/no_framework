987f06b1b80d9b48fb21af560060b1dbad262482
Sameh R. Labib <schoolbuscompany@gmail.com>
2015-08-14 12:12:44 -0400

    Added items to Admin Menu index.php

--- web/helpme/admin/index.php
+++ web/helpme/admin/index.php
@@ -14,12 +14,28 @@ if (!$session->is_logged_in()) { redirect_to("login.php"); }
 <h2>Menu</h2>
 
 <?php echo output_message($message); ?>
+
+<p><a href="logout.php">Logout</a></p>
+
+<p><a href="logfile.php">View Log file</a></p>
+
+<h3>Videos</h3>
+
 <ul>
-  <li><a href="add_video.php">Add Video to Database</a></li>
-  <li><a href="list_photos.php">List Photos</a></li>
+  <li><a href="add_video.php">Add Admin Videos</a></li>
+  <li><a href="list_videos.php">Manage Videos and Their Comments</a></li>
+</ul>
+
+<h3>Photo Gallery</h3>
+
+<ul>
   <li><a href="photo_upload.php">Photo Upload</a></li>
-  <li><a href="logfile.php">View Log file</a></li>
-  <li><a href="logout.php">Logout</a></li>
+  <li><a href="list_photos.php">List Photos</a></li>
+  <li></li>
+</ul>
+
+<h3>E-mailing</h3>
+<ul>
   <li><a href="sendemail.php">Test Email Sending</a></li>
   <li><a href="sendemail2.php">Test PHPMailer Email Sending</a></li>
 </ul>
