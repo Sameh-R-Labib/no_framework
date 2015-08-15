7c5b7f9572cc3f9440e2eb27115bba33c2021854
Sameh R. Labib <schoolbuscompany@gmail.com>
2015-08-15 14:59:13 -0400

    Fixed something I missed again

--- web/helpme/admin/index.php
+++ web/helpme/admin/index.php
@@ -9,7 +9,7 @@ if (!$session->is_logged_in()) { redirect_to("login.php"); }
 
 <?php include_layout_template('admin_header.php'); ?>
 
-<a href="../index.php">&laquo; Back</a><br /><br />
+<a href="../../index.php">&laquo; Back</a><br /><br />
 
 <h2>Menu</h2>
 
