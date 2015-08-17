36c9f2712619795aed5579628ba175f3846d0a4d
Sameh R. Labib <schoolbuscompany@gmail.com>
2015-08-17 13:18:47 -0400

    GeneratePrivacyPolicy.com added to footers

--- web/helpme/layouts/admin_footer.php
+++ web/helpme/layouts/admin_footer.php
@@ -1,5 +1,5 @@
     </div>
-    <div id="footer">Copyright <?php echo date("Y", time()); ?>, SAMEH R LABIB, LLC</div>
+    <div id="footer">Copyright <?php echo date("Y", time()); ?>, SAMEH R LABIB, LLC | <a target="_blank" href="http://www.generateprivacypolicy.com/privacy/view/1lFBvQ">Privacy policy</a></div>
   </body>
 </html>
 <?php if(isset($database)) { $database->close_connection(); } ?>
